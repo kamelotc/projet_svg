690ce36e65f1533ae2b5b144eba35f3e03a37eca
YoannDeStena <ydestena@gmail.com>
2025-10-08 11:11:13 +0200

    feat: Ajout d'une façon simple de vérifier si le caractère donné est une forme valide

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void generateShape(FILE *svg, const char shape, const int width, const int height, char color[6]) {
     if (color == NULL)
@@ -21,24 +22,29 @@ int main(void) {
 
     printf("R = Rectangle");
     printf("\nC = Cercle");
+    printf("\nE = Ellipse");
     printf("\nChoisissez la forme : ");
-    if (scanf("%c",&shape) != 1 && toupper(shape) != 'C' && toupper(shape) != 'R') {
+    if (scanf("%c",&shape) != 1) {
         fputs ("ERROR: Invalid input.\n", stderr);
-        return 1;
+        return EXIT_FAILURE;
     }
 
     shape = toupper(shape);
+    if (strpbrk(&shape, "CRE") == 0) {
+        fputs ("ERROR: Invalid input. Must be one of the characters in the given list.\n", stderr);
+        return EXIT_FAILURE;
+    }
 
     printf("Choisissez la largeur : ");
     if (scanf("%d",&width) != 1 && width <= 0) {
         fputs ("ERROR: Invalid input.\n", stderr);
-        return 1;
+        return EXIT_FAILURE;
     }
 
     printf("\nChoisissez la hauteur : ");
     if (scanf("%d",&height) != 1 && height <= 0) {
         fputs ("ERROR: Invalid input.\n", stderr);
-        return 1;
+        return EXIT_FAILURE;
     }
 
     fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\">", width, height, width, height);
