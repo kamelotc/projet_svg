f5dc85294b94977b7196a86bef784c4728d57699
YoannDeStena <ydestena@gmail.com>
2025-10-08 10:09:25 +0200

    feat: Ajout de la possibilité de créer des carrés/rectangles avec la hauteur/largeur donnée

--- main.c
+++ main.c
@@ -4,7 +4,23 @@
 int main(void) {
     const FILE *svg = fopen("mon_dessin.svg", "w");
 
-    fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1\" height=\"1\" viewBox=\"0 0 1 1\">");
-    fprintf(svg, "<rect width=\"1\" height=\"1\" x=\"0\" fill=\"#ff0000\"/>");
+    int largeur = 0;
+    int hauteur = 0;
+
+    printf("Choisissez la largeur : ");
+    if (scanf("%d",&largeur) != 1 && largeur <= 0) {
+        fputs ("ERROR: Invalid input.\n", stderr);
+        return 1;
+    }
+
+    printf("\nChoisissez la hauteur : ");
+    if (scanf("%d",&hauteur) != 1 && hauteur <= 0) {
+        fputs ("ERROR: Invalid input.\n", stderr);
+        return 1;
+    }
+    fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\">", largeur, hauteur, largeur, hauteur);
+    fprintf(svg, "<rect width=\"%d\" height=\"%d\" x=\"0\" fill=\"#ff0000\"/>", largeur, hauteur);
     fprintf(svg, "</svg>");
+
+    return EXIT_SUCCESS;
 }
