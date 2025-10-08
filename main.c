0d69b0c97987a2175a1d1869cae2cff7c057b25d
YoannDeStena <ydestena@gmail.com>
2025-10-08 10:36:26 +0200

    feat: Ajout de la création d'un cercle et de la capacité de choisir une forme à créer

--- main.c
+++ main.c
@@ -4,8 +4,17 @@
 int main(void) {
     const FILE *svg = fopen("mon_dessin.svg", "w");
 
+    char forme;
     int largeur = 0;
     int hauteur = 0;
+
+    printf("R = Rectangle");
+    printf("\nC = Cercle");
+    printf("\nChoisissez la forme : ");
+    if (scanf("%c",&forme) != 1 && forme != 'C' && forme != 'R') {
+        fputs ("ERROR: Invalid input.\n", stderr);
+        return 1;
+    }
 
     printf("Choisissez la largeur : ");
     if (scanf("%d",&largeur) != 1 && largeur <= 0) {
@@ -18,8 +27,11 @@ int main(void) {
         fputs ("ERROR: Invalid input.\n", stderr);
         return 1;
     }
+
     fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\">", largeur, hauteur, largeur, hauteur);
-    fprintf(svg, "<rect width=\"%d\" height=\"%d\" x=\"0\" fill=\"#ff0000\"/>", largeur, hauteur);
+    if (forme == 'R')
+        fprintf(svg, "<rect width=\"%d\" height=\"%d\" x=\"0\" fill=\"#ff0000\"/>", largeur, hauteur);
+    else fprintf(svg, "<circle cx=\"%d\" cy=\"%d\" x=\"0\" r=\"%d\" fill=\"#ff0000\"/>", largeur/2, hauteur/2, largeur/2);
     fprintf(svg, "</svg>");
 
     return EXIT_SUCCESS;
