1f2af05ed7eb025a01ad2f802a17a35c6b96b5b8
YoannDeStena <ydestena@gmail.com>
2025-10-08 10:58:30 +0200

    refactor: Déplacement de la génération des formes dans une fonction propre à ce but et utilisation des switchs pour reconnaitre la forme à créer

--- main.c
+++ main.c
@@ -1,37 +1,48 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+void generateShape(FILE *svg, const char shape, const int width, const int height, char color[6]) {
+    if (color == NULL)
+        color = "ff0000";
+    switch (shape) {
+        case 'C': fprintf(svg, "<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"#%s\"/>", width/2, height/2, width/2, color); break;
+        case 'R': fprintf(svg, "<rect width=\"%d\" height=\"%d\" x=\"0\" fill=\"#%s\"/>", width, height, color); break;
+        default: fputs ("ERROR: This shape could not be recognized despite passing the first check?", stderr); break;
+    }
+}
 
 int main(void) {
     const FILE *svg = fopen("mon_dessin.svg", "w");
 
-    char forme;
-    int largeur = 0;
-    int hauteur = 0;
+    char shape;
+    int width = 0;
+    int height = 0;
 
     printf("R = Rectangle");
     printf("\nC = Cercle");
     printf("\nChoisissez la forme : ");
-    if (scanf("%c",&forme) != 1 && forme != 'C' && forme != 'R') {
+    if (scanf("%c",&shape) != 1 && toupper(shape) != 'C' && toupper(shape) != 'R') {
         fputs ("ERROR: Invalid input.\n", stderr);
         return 1;
     }
 
+    shape = toupper(shape);
+
     printf("Choisissez la largeur : ");
-    if (scanf("%d",&largeur) != 1 && largeur <= 0) {
+    if (scanf("%d",&width) != 1 && width <= 0) {
         fputs ("ERROR: Invalid input.\n", stderr);
         return 1;
     }
 
     printf("\nChoisissez la hauteur : ");
-    if (scanf("%d",&hauteur) != 1 && hauteur <= 0) {
+    if (scanf("%d",&height) != 1 && height <= 0) {
         fputs ("ERROR: Invalid input.\n", stderr);
         return 1;
     }
 
-    fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\">", largeur, hauteur, largeur, hauteur);
-    if (forme == 'R')
-        fprintf(svg, "<rect width=\"%d\" height=\"%d\" x=\"0\" fill=\"#ff0000\"/>", largeur, hauteur);
-    else fprintf(svg, "<circle cx=\"%d\" cy=\"%d\" x=\"0\" r=\"%d\" fill=\"#ff0000\"/>", largeur/2, hauteur/2, largeur/2);
+    fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\">", width, height, width, height);
+    generateShape(svg, shape, width, height, NULL);
     fprintf(svg, "</svg>");
 
     return EXIT_SUCCESS;
